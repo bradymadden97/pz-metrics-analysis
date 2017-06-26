44286c508c7af153c9848b209b1adf560c66b97a
Brady Madden <bradymadden97@gmail.com>
2017-06-26 14:48:26 -0400

    reorg. express serv

--- dashboard/app.js
+++ dashboard/app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-var graph_data = require("./graph_data.json")
+app.use(express.static(__dirname + '/public'));
+
 
 app.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'));
+	res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(8000, function(){
