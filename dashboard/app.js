27dc9654bbc597e3bb6f9cbbaacf4a1696843587
Brady Madden <bradymadden97@gmail.com>
2017-06-27 16:05:39 -0400

    all views handlebars

--- dashboard/app.js
+++ dashboard/app.js
@@ -25,7 +25,9 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname + '/public/index.html'));
+	res.render('index', {
+		graphNames: graph_data,
+	});
 });
 app.get("/graph/:graphName", function(req, res) {
 	data = graph_data[req.params.graphName];
